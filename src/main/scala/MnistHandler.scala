8d1ef07e3ecfc7b49516ba4e67d3d9dfc998e0dc
Andreas Vind Jensen <andreasvindjensen@gmail.com>
2025-10-21 14:43:07 +0200

    Fixed formatting

--- src/main/scala/MnistHandler.scala
+++ src/main/scala/MnistHandler.scala
@@ -5,17 +5,17 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 
 /* TODO:
-* - Add random selection of images
-* - consider converting directly to binary using thresholds
-*/
+ * - Add random selection of images
+ * - consider converting directly to binary using thresholds
+ */
 
 class MnistHandler(val Path: String) {
   var images: Array[Array[Array[Byte]]] = Array.ofDim[Byte](60000, 28, 28)
-  var labels: Array[Byte] = Array.ofDim[Byte](60000)
+  var labels: Array[Byte]               = Array.ofDim[Byte](60000)
 
   def readMnist(): Unit = {
-    //val imagePath = "/home/andreas/Documents/agile-hw/MNIST_ORG/train-images.idx3-ubyte"
-    val imagePath = this.Path + "train-images.idx3-ubyte"
+    // val imagePath = "/home/andreas/Documents/agile-hw/MNIST_ORG/train-images.idx3-ubyte"
+    val imagePath   = this.Path + "train-images.idx3-ubyte"
     var ubyteImages = new File(imagePath)
     val imageStream = new java.io.FileInputStream(ubyteImages)
 
@@ -23,13 +23,12 @@ class MnistHandler(val Path: String) {
 
     try {
       imageStream.skip(16) // skip header
-      for (i <- 0 until 60000) {
+      for (i <- 0 until 60000)
         for (j <- 0 until (28 * 28)) {
           imageStream.read(dataBuffer)
-          val pixelVal = (dataBuffer(0) & 0xFF)
+          val pixelVal = dataBuffer(0) & 0xff
           this.images(i)(j / 28)(j % 28) = pixelVal.toByte
         }
-      }
     } catch {
       case e: IOException => e.printStackTrace()
     } finally {
@@ -49,7 +48,7 @@ class MnistHandler(val Path: String) {
   }
 
   def readLabels(): Unit = {
-    val labelPath = this.Path + "train-labels.idx1-ubyte"
+    val labelPath   = this.Path + "train-labels.idx1-ubyte"
     var ubyteLabels = new File(labelPath)
     val labelStream = new java.io.FileInputStream(ubyteLabels)
 
@@ -63,33 +62,27 @@ class MnistHandler(val Path: String) {
       }
     } catch {
       case e: IOException => e.printStackTrace()
-    } finally {
+    } finally
       labelStream.close()
-    }
   }
 
-  def countLabelOccurrences(): Map[Byte, Int] = {
+  def countLabelOccurrences(): Map[Byte, Int] =
     this.labels.groupBy(identity).view.mapValues(_.length).toMap
-  }
 
   def Sort: Unit = {
     val combined = this.labels.zip(this.images)
-    val sorted = combined.sortBy(_._1)
+    val sorted   = combined.sortBy(_._1)
     this.labels = sorted.map(_._1)
     this.images = sorted.map(_._2)
   }
 
   def saveToBmp(image: Array[Array[Byte]], name: String): Unit = {
     val image1 = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY)
-    
-    for (i <- 0 until 28) {
-      for (j <- 0 until 28) {
+
+    for (i <- 0 until 28)
+      for (j <- 0 until 28)
         image1.setRGB(j, i, images(0)(i)(j) << 16 | images(0)(i)(j) << 8 | images(0)(i)(j))
-      }
-    }
 
     ImageIO.write(image1, "bmp", new File("/home/andreas/Documents/agile-hw/MNIST_ORG/" + name + ".bmp"))
   }
 }
-
-
