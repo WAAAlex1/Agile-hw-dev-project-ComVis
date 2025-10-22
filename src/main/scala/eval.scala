620cad49d6a656a6ae6ee8971b1b03dfed1c1214
sofusham <s224039@dtu.dk>
2025-10-22 12:26:19 +0200

    after scalafmt cmd

--- src/main/scala/eval.scala
+++ src/main/scala/eval.scala
@@ -16,20 +16,20 @@ class Eval(val N: Int, val WIDTH: Int = 10) extends Module {
   val initScore = io.c_scores(0)
   val initIdx   = 0.U(idxWidth.W)
 
-  val (bestScoreComb, bestIdxComb) = io.c_scores.zipWithIndex.drop(1).foldLeft((initScore, initIdx)) {
-    case ((maxScore, maxIdx), (currScore, currIdx)) =>
-      val currIdxU = currIdx.U(idxWidth.W)
-      val takeCurr = currScore > maxScore
+  val (bestScoreComb, bestIdxComb) =
+    io.c_scores.zipWithIndex.drop(1).foldLeft((initScore, initIdx)) { case ((maxScore, maxIdx), (currScore, currIdx)) =>
+      val currIdxU    = currIdx.U(idxWidth.W)
+      val takeCurr    = currScore > maxScore
       val newMaxScore = Mux(takeCurr, currScore, maxScore)
       val newMaxIdx   = Mux(takeCurr, currIdxU, maxIdx)
       (newMaxScore, newMaxIdx)
-  }
+    }
 
   val bestScoreReg = RegInit(0.U(WIDTH.W))
   val bestIdxReg   = RegInit(0.U(idxWidth.W))
   val validReg     = RegInit(false.B)
 
-  when (io.in_valid) {
+  when(io.in_valid) {
     bestScoreReg := bestScoreComb
     bestIdxReg   := bestIdxComb
   }
